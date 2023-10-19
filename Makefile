4d3d71cbb948616769e273897d2ad2cc70fa1eca
pedrooc16 <pedro1668@hotmail.com>
2023-10-19 15:32:47 +0100

    make done

--- Makefile
+++ Makefile
@@ -1,14 +1,33 @@
-CLIENT = client 
-SERVER = server
-CC = CC
+Name_C = client 
+Name_S = server
+CC = cc
 CFLAGS = -Wall -Werror -Wextra -g
-//g for debugging
+Rm = rm -rf
 
-all: $S(SERVER) $(CLIENT)
+# Define source files
+SRC_C = client.c
+SRC_S = server.c
 
-$(NAME): all
+# Define object files
+OBJ_C = $(SRC_C:.c=.o)
+OBJ_S = $(SRC_S:.c=.o)
 
-$(SERVER)
-	$(CC) - $(CFLAGS) 
+all: $(Name_C) $(Name_S)
 
-$(CLIENT)
+$(Name_C): $(OBJ_C)
+	$(CC) $(CFLAGS) $(OBJ_C) -o $(Name_C)
+
+$(Name_S):	$(OBJ_S)
+	$(CC) $(CFLAGS) $(OBJ_S) -o $(Name_S)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c 
+
+clean:
+	$(Rm) $(OBJ_S) $(OBJ_C)
+
+fclean:	clean
+	$(Rm) $(Name_C) $(Name_S)
+
+re:
+	fclean	all
--- client.c
+++ client.c
@@ -6,12 +6,16 @@
 /*   By: pecosta- <pecosta-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 15:20:31 by pecosta-          #+#    #+#             */
-/*   Updated: 2023/05/25 17:07:58 by pecosta-         ###   ########.fr       */
+/*   Updated: 2023/10/19 15:04:24 by pecosta-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 int	main(int argc, char **argv)
 {
-	if (argc != 3)
-		write(Sorry, couldn't get anything");
+	if (argc != 3 && argv != NULL)
+		write(1, "Sorry, couldn't get anything", 20);
+	else
+		write(1, "Hello\n", 10);		
 }
